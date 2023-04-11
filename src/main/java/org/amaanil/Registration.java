bef55895f77d0b8fc188f5d4af28bb5f29530eba
AmaaniL <malfait99+github@gmail.com>
2023-04-11 13:35:07 +0200

    Fix loop after entering dr variable

--- src/main/java/org/amaanil/Registration.java
+++ src/main/java/org/amaanil/Registration.java
@@ -8,51 +8,70 @@ import java.util.Scanner;
 
 public class Registration {
 
-public void registration(){
-    {
-        Scanner inputScanner = new Scanner(System.in);
-        List<Visitors> visitors = new ArrayList<>();
+    public void registration() {
+        {
+            Scanner inputScanner = new Scanner(System.in);
+            List<Visitors> visitors = new ArrayList<>();
 
-        Scanner inputscanner = new Scanner(System.in);
+            Scanner inputscanner = new Scanner(System.in);
 
-        System.out.println("Register or report?");
-        String userInput = inputscanner.nextLine();
+            boolean running = true;
+            while (running) {
+            System.out.println("Register or report?");
+            String userInput = inputscanner.nextLine();
 
-        userInput = userInput.toLowerCase();
+            userInput = userInput.toLowerCase();
 
-        switch (userInput) {
-            case "register" -> {
-                boolean running = true;
-                while (running) {
-                    System.out.print("Enter name or 'q' to quit: ");
-                    String name = inputScanner.nextLine();
-                    if ("q".equalsIgnoreCase(name)) {
-                        running = false;
-                    } else {
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                        String formattedDateTime = LocalDateTime.now().format(formatter);
-                        Visitors visitor = new Visitors(name, formattedDateTime);
-                        visitors.add(visitor);
+            switch (userInput) {
+                case "register" -> {
 
+                        System.out.print("Enter name or 'q' to quit: ");
+                        String name = inputScanner.nextLine();
+                        if ("q".equalsIgnoreCase(name)) {
+                            running = false;
+                        } else {
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                            String formattedDateTime = LocalDateTime.now().format(formatter);
+                            Visitors visitor = new Visitors(name, formattedDateTime);
+                            visitors.add(visitor);
+
+                            System.out.println("Choose between doctor Mario or doctor Frankenstein");
+                            String doctor = inputScanner.nextLine();
+
+                            if (("Mario".equalsIgnoreCase(doctor))) {
+                                System.out.println("Mario");
+                            } else if ("Frankenstein".equalsIgnoreCase(doctor)) {
+                                System.out.println("Frankenstein");
+                            } else {
+                                System.out.println("Wrong input. Type Mario or Frankenstein.");
+                            }
+
+
+                        }
 
                     }
 
-                }
-                for (Visitors names:  visitors
-                ) {
-                    System.out.println(names.getName() + " " + names.getTimestamp());
+
+
+                case "report" -> {
+
+                    if (visitors.isEmpty()) {
+                        System.out.println("There have been no visitors yet.");
+                    } else {
+                        for (Visitors names : visitors
+                        ) {
+                            System.out.println(names.getName() + " " + names.getTimestamp());
+                            running = false;
+
+                        }
+                    }
 
                 }
-
+                default -> System.out.println("Wrong input");
             }
-            case "report" -> {
-                System.out.println("You chose report");
-
             }
-            default -> System.out.println("Wrong input");
         }
-    }
 
-}
+    }
 
 }
